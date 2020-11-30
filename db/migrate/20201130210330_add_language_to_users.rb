60d1324ed2de133e64e63c68f91d44c352ea23f4
Gabriel Fontes <gabrielferroroque@hotmail.com>
2020-11-30 18:47:49 -0300

    Core features db migrations and models created

--- db/migrate/20201130210330_add_language_to_users.rb
+++ db/migrate/20201130210330_add_language_to_users.rb
@@ -1,4 +1,4 @@
-class AddLanguageRefToUsers < ActiveRecord::Migration[6.0]
+class AddLanguageToUsers < ActiveRecord::Migration[6.0]
   def change
     add_reference :users, :language, null: false, foreign_key: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_204206) do
+ActiveRecord::Schema.define(version: 2020_11_30_211710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "interests", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_interests_on_user_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "chat_room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "chat_room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chat_room_id"], name: "index_participants_on_chat_room_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
+  create_table "user_languages", force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.bigint "user_id", null: false
+    t.string "level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["language_id"], name: "index_user_languages_on_language_id"
+    t.index ["user_id"], name: "index_user_languages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +72,23 @@ ActiveRecord::Schema.define(version: 2020_11_30_204206) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "country"
+    t.bigint "language_id", null: false
+    t.string "about_me"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["language_id"], name: "index_users_on_language_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "interests", "users"
+  add_foreign_key "messages", "chat_rooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "participants", "chat_rooms"
+  add_foreign_key "participants", "users"
+  add_foreign_key "user_languages", "languages"
+  add_foreign_key "user_languages", "users"
+  add_foreign_key "users", "languages"
 end
