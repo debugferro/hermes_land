38a32fd5483a574c698933b97939ec1873cb2380
Gabriel Fontes <gabrielferroroque@hotmail.com>
2020-12-01 16:59:31 -0300

    work in progress, backup

---
+++ app/controllers/interests_controller.rb
@@ -0,0 +1,22 @@
+class InterestsController < ApplicationController
+
+  def index
+    @interest = Interest.new
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
